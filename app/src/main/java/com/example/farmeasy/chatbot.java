ab6f4c755376b8a97dfd6cadbfb2f01e00987341
GUNTUKU NARESH <guntuku001@gmail.com>
2024-02-27 15:10:12 +0530

    Added Sample ChatBot

--- app/src/main/java/com/example/farmeasy/chatbot.java
+++ app/src/main/java/com/example/farmeasy/chatbot.java
@@ -3,12 +3,27 @@ package com.example.farmeasy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class chatbot extends AppCompatActivity {
-
+    private WebView webView;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot);
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.chatbase.co/chatbot-iframe/1OZmP9srAAKQiJPROCoUG");
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
